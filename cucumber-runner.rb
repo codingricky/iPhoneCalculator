269677fad23ceccdb938c9289bfcdec44a66e0b5
Ricky Yim <ricky@dius.com.au>
2014-10-21 23:40:13 +1100

    mods to the runner

--- cucumber-runner.rb
+++ cucumber-runner.rb
@@ -1,9 +1,21 @@
 require 'fileutils'
+require 'rubygems'
 
 TARGETS = []
 TARGETS << 'iPad 2 (8.0 Simulator)'
 TARGETS << 'iPhone 6 (8.0 Simulator)'
 TARGETS << 'iPhone 4s (8.0 Simulator)'
+
+version = ">= 0"
+
+if ARGV.first
+  str = ARGV.first
+  str = str.dup.force_encoding("BINARY") if str.respond_to? :force_encoding
+  if str =~ /\A_(.*)_\z/
+    version = $1
+    ARGV.shift
+  end
+end
 
 TARGETS.each do |target|
 	ENV['DEVICE_TARGET'] = target
@@ -12,5 +24,7 @@ TARGETS.each do |target|
 	FileUtils.mkdir_p(screenshot_path)
 
 	puts "Running using #{target} storing screenshots #{screenshot_path}"
-	system('cucumber')
+
+	gem 'cucumber', version
+	load Gem.bin_path('cucumber', 'cucumber', version)
 end
